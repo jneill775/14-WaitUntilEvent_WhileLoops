156d456062d74cd5108d3c125e2e76fabeaaa4ae
jneill775 <neilljs@rose-hulman.edu>
2019-01-09 16:19:16 -0500

    done

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -3,19 +3,24 @@ This module demonstrates lets you practice INPUT from the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and John Neill.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-
+import math
 def main():
     """ TESTs the functions in this module (by calling them). """
-    double_a_float()
-    print_an_integer_many_times()
-    print_an_integer_many_times_on_one_row()
+    #double_a_float()
+    #print_an_integer_many_times()
+    #print_an_integer_many_times_on_one_row()
     input_it_all()
 
 
 def double_a_float():
+
+    x = int(input('Enter a number: '))
+    dubski = 2 * x
+    print(dubski)
+
     """
     What comes in: Nothing.
     What goes out: Nothing (i.e. None)
@@ -31,12 +36,20 @@ def double_a_float():
          -6.28
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
 
 def print_an_integer_many_times():
+
+    integer = int(input('Enter an integer: '))
+    d = 2 * integer
+    i = 0
+    while i < integer:
+        print (d)
+        i+=1
+
     """
     What comes in: Nothing.
     What goes out: Nothing (i.e. None)
@@ -62,12 +75,20 @@ def print_an_integer_many_times():
          10
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
 
 def print_an_integer_many_times_on_one_row():
+
+
+    integer = int(input('Enter an integer: '))
+    d = 2 * integer
+    i = 0
+    print(str(d) * integer)
+
+
     """
     Same as the previous problem, but print the numbers
     on a single line with no spaces in between them.
@@ -81,7 +102,7 @@ def print_an_integer_many_times_on_one_row():
          1010101010
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # HINT: One way to print on a SINGLE line is to build up a string
@@ -90,6 +111,20 @@ def print_an_integer_many_times_on_one_row():
 
 
 def input_it_all():
+
+    lexusls400 = float(input('Enter a positive floating point number: '))
+    lexusls430 = int(input('Enter a positive integer: '))
+    lexusls460 = input('Enter a string: ')
+    flexus = math.sqrt(lexusls400)
+    i = 0
+    j = 0
+    while i < lexusls430:
+        print(str(flexus))
+        i += 1
+    while j < lexusls430:
+        print(lexusls460)
+        j += 1
+
     """
     What comes in: Nothing.
     What goes out: Nothing (i.e. None)
@@ -121,7 +156,7 @@ def input_it_all():
          Peace & Love.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
