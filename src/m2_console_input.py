f95873d6e7cc49699ce46feb1cc25785bd6c5b6e
jneill775 <neilljs@rose-hulman.edu>
2019-01-09 16:20:04 -0500

    done

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -9,9 +9,9 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 import math
 def main():
     """ TESTs the functions in this module (by calling them). """
-    #double_a_float()
-    #print_an_integer_many_times()
-    #print_an_integer_many_times_on_one_row()
+    double_a_float()
+    print_an_integer_many_times()
+    print_an_integer_many_times_on_one_row()
     input_it_all()
 
 
